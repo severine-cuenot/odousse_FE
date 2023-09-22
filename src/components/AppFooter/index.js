99e7555b838eeae8e86018ac8a13407f873a2b99
Nine Poppy <nine.injections@gmail.com>
2023-09-22 11:01:22 +0200

    :wrench: config : footer for desktop ok

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -7,34 +7,50 @@ function AppFooter() {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <header className="footer__header">
-          <Link to="/">Ôdousse</Link>
-        </header>
-        <nav className="footer__navigation">
-          <div className="footer__main-nav">
-            <h2 className="footer__section-title">
-              Ôdousse
-            </h2>
-            <ul className="footer__section-list">
-              <li><Link to="/about">À propos</Link></li>
-            </ul>
+        <div className="footer__main-nav">
+          {/* HEADER */}
+          <div>
+            <header className="footer__header">
+              <Link to="/">Ôdousse</Link>
+            </header>
+          </div>
+          <div>
+            <nav className="footer__navigation">
+              <h2 className="footer__section-title">
+                Ôdousse
+              </h2>
+              <ul className="footer__section-list">
+                <li><Link to="/about">À propos</Link></li>
+              </ul>
+            </nav>
+          </div>
+          {/* TOOLS */}
+          <div>
             <h2 className="footer__section-title">
               Outils
             </h2>
-            <ul className="footer__section-list">
-              <li><Link to="/search">Recherche détaillée</Link></li>
-              <li><Link to="/lexicon">Lexique</Link></li>
-            </ul>
+            <nav className="footer__navigation">
+              <ul className="footer__section-list">
+                <li><Link to="/search">Recherche détaillée</Link></li>
+                <li><Link to="/lexicon">Lexique</Link></li>
+              </ul>
+            </nav>
+          </div>
+          {/* LEGAL */}
+          <div>
             <h2 className="footer__section-title">
               Légal
             </h2>
-            <ul className="footer__section-list">
-              <li><Link to="/cgu">CGU</Link></li>
-              <li><Link to="/privacy">Politique de confidentialité</Link></li>
-              <li><Link to="/credits">Crédits</Link></li>
-            </ul>
+            <nav className="footer__navigation">
+              <ul className="footer__section-list">
+                <li><Link to="/cgu">CGU</Link></li>
+                <li><Link to="/privacy">Politique de confidentialité</Link></li>
+                <li><Link to="/credits">Crédits</Link></li>
+              </ul>
+            </nav>
           </div>
-        </nav>
+        </div>
+        {/* COPYRIGHT */}
         <div className="footer__copyright">
           <p>Tous droits réservés &copy; NineInjections {year}</p>
         </div>
