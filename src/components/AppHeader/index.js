6f2c1e0ce6e6b92cb0a75b94c7e979b96804ae87
Nine Poppy <nine.injections@gmail.com>
2023-09-22 11:51:37 +0200

    :hammer: fix : removing mixin because I don't understand well how it works. Will refacto later if I can figure out

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -21,36 +21,44 @@ function AppHeader({ handleChangeFont }) {
   const svgClass = isExpanded ? 'nav-menu__burger-svg-cross' : 'nav-menu__burger-svg';
   return (
     <div>
-      <nav className="nav-menu" role="navigation" aria-label="Menu de navigation">
-        <div className="nav-menu__block">
-          <Link to="/" title="Retourner à la page d'accueil" className="nav-menu__logo">Ôdousse</Link>
-          <button
-            title="Ouvrir le menu"
-            aria-label="Ouvrir le menu"
-            className="nav-menu__burger-button"
-            type="button"
-            aria-expanded={isExpanded ? 'true' : 'false'}
-            onClick={handleClick}
-          >
-            <svg
-              data-src={svgImg}
-              alt="{svgImg}"
-              className={svgClass}
-            />
-            <span className="nav-menu__burger-button--text">Menu</span>
-          </button>
-        </div>
-        <ul className={isExpanded ? 'nav-menu__expanded' : 'nav-menu__not-expanded'}>
-          <ToggleDyslexia onClick={handleChangeFont} />
-          <div className="nav-menu__expanded--buttons">
-            <li onClick={handleClick}><Link to="/login"><Button label="S'inscrire" buttonStyle="simple" /></Link></li>
-            <li onClick={handleClick}><Link to="/login"><Button label="Connexion" buttonStyle="empty" /></Link></li>
+      <div className="nav__mobile">
+        <nav className="nav-menu" role="navigation" aria-label="Menu de navigation">
+          <div className="nav-menu__block">
+            <Link to="/" title="Retourner à la page d'accueil" className="nav-menu__logo">Ôdousse</Link>
+            <button
+              title="Ouvrir le menu"
+              aria-label="Ouvrir le menu"
+              className="nav-menu__burger-button"
+              type="button"
+              aria-expanded={isExpanded ? 'true' : 'false'}
+              onClick={handleClick}
+            >
+              <svg
+                data-src={svgImg}
+                alt="{svgImg}"
+                className={svgClass}
+              />
+              <span className="nav-menu__burger-button--text">Menu</span>
+            </button>
           </div>
-          <li className="nav-menu__expanded--element" onClick={handleClick}><Link to="/lexicon">Glossaire</Link></li>
-          <li className="nav-menu__expanded--element" onClick={handleClick}><Link to="/about">À propos</Link></li>
-        </ul>
-      </nav>
-      <hr className="nav-menu__border-block" />
+          <ul className={isExpanded ? 'nav-menu__expanded' : 'nav-menu__not-expanded'}>
+            <ToggleDyslexia onClick={handleChangeFont} />
+            <div className="nav-menu__expanded--buttons">
+              <li onClick={handleClick}><Link to="/login"><Button label="S'inscrire" buttonStyle="simple" /></Link></li>
+              <li onClick={handleClick}><Link to="/login"><Button label="Connexion" buttonStyle="empty" /></Link></li>
+            </div>
+            <li className="nav-menu__expanded--element" onClick={handleClick}><Link to="/lexicon">Glossaire</Link></li>
+            <li className="nav-menu__expanded--element" onClick={handleClick}><Link to="/about">À propos</Link></li>
+          </ul>
+        </nav>
+        <hr className="nav-menu__border-block" />
+      </div>
+      <div className="nav__desktop">
+        <nav className="nav-menu" role="navigation" aria-label="Menu de navigation">
+          Menu desktop
+        </nav>
+        <hr className="nav-menu__border-block" />
+      </div>
     </div>
   );
 }
