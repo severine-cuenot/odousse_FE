fd0ebff5617a138448ff47292009c08d0fb5c909
Nine Poppy <nine.injections@gmail.com>
2023-09-28 16:25:53 +0200

    :rocket: feat: started hero component

---
+++ src/components/HeroSearch/index.js
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import Button from '../button';
+import './style.scss';
+
+function HeroSearch() {
+  return (
+    <div className="hero__panel">
+      <h1 className="hero__title">Votre Recherche</h1>
+      <section className="hero__section">
+        <form>
+          <fieldset>
+            <div className="hero__form">
+              <legend>Poissons</legend>
+              <div className="hero__input">
+                <label htmlFor="litres" className="hero__input-label">Litrage de votre aquarium</label>
+                <span className="hero__input-span"><input type="number" name="litres" id="litres" className="hero__input-box" /> Litres </span>
+              </div>
+              <div className="hero__input">
+                <label htmlFor="ph" className="hero__input-label">PH de votre eau</label>
+                <span className="hero__input-span"><input type="number" name="ph" id="ph" className="hero__input-box" /> PH</span>
+              </div>
+              <Button label="Lancer la recherche" type="submit" />
+            </div>
+          </fieldset>
+        </form>
+      </section>
+    </div>
+  );
+}
+
+export default HeroSearch;
