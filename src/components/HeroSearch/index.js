79ecd994ad92781762031aebc6a4ad9d2533b6eb
Nine Poppy <nine.injections@gmail.com>
2023-09-28 19:00:23 +0200

    :art: style : added some CSS

--- src/components/HeroSearch/index.js
+++ src/components/HeroSearch/index.js
@@ -10,16 +10,46 @@ function HeroSearch() {
         <form>
           <fieldset>
             <div className="hero__form">
-              <legend>Poissons</legend>
-              <div className="hero__input">
-                <label htmlFor="litres" className="hero__input-label">Litrage de votre aquarium</label>
-                <span className="hero__input-span"><input type="number" name="litres" id="litres" className="hero__input-box" /> Litres </span>
+              <div className="hero__form-header">
+                <div className="active">Poissons</div>
+                <div>Invertébrés</div>
+                <div>Plantes</div>
               </div>
-              <div className="hero__input">
-                <label htmlFor="ph" className="hero__input-label">PH de votre eau</label>
-                <span className="hero__input-span"><input type="number" name="ph" id="ph" className="hero__input-box" /> PH</span>
+              <div className="hero__form-body">
+                <div className="hero__form-fish active">
+                  <div className="hero__input">
+                    <label htmlFor="litres" className="hero__input-label">Litrage de votre aquarium</label>
+                    <span className="hero__input-span"><input type="number" name="litres" id="litres" className="hero__input-box" /> Litres </span>
+                  </div>
+                  <div className="hero__input">
+                    <label htmlFor="ph" className="hero__input-label">PH de votre eau</label>
+                    <span className="hero__input-span"><input type="number" name="ph" id="ph" className="hero__input-box" /> PH</span>
+                  </div>
+                  <Button label="Lancer la recherche" type="submit" />
+                </div>
+                <div className="hero__form-invertebrate">
+                  <div className="hero__input">
+                    <label htmlFor="litres" className="hero__input-label">Litrage de votre aquarium</label>
+                    <span className="hero__input-span"><input type="number" name="litres" id="litres" className="hero__input-box" /> Litres </span>
+                  </div>
+                  <div className="hero__input">
+                    <label htmlFor="ph" className="hero__input-label">PH de votre eau</label>
+                    <span className="hero__input-span"><input type="number" name="ph" id="ph" className="hero__input-box" /> PH</span>
+                  </div>
+                  <Button label="Lancer la recherche" type="submit" />
+                </div>
+                <div className="hero__form-plant">
+                  <div className="hero__input">
+                    <label htmlFor="litres" className="hero__input-label">Litrage de votre aquarium</label>
+                    <span className="hero__input-span"><input type="number" name="litres" id="litres" className="hero__input-box" /> Litres </span>
+                  </div>
+                  <div className="hero__input">
+                    <label htmlFor="ph" className="hero__input-label">PH de votre eau</label>
+                    <span className="hero__input-span"><input type="number" name="ph" id="ph" className="hero__input-box" /> PH</span>
+                  </div>
+                  <Button label="Lancer la recherche" type="submit" />
+                </div>
               </div>
-              <Button label="Lancer la recherche" type="submit" />
             </div>
           </fieldset>
         </form>
