5568ef27a7972c1bda58b7493da0a45ccd02e487
Séverine Cuenot <nine.injections@gmail.com>
2024-07-30 11:38:33 +0200

    :rocket: feat: testing bootstrap navbar

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,8 +1,46 @@
+// Bootstrap imports
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+// Other imports
 import './style.scss';
 
 function AppHeader() {
   return (
-    <div>Ôdousse</div>
+    <Navbar expand="md" sticky="top" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/img/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Ôdousse
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Accueil</Nav.Link>
+            <Nav.Link href="#link">Liens utiles</Nav.Link>
+            <NavDropdown title="Recherche" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Par litrage</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Par biotope
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Par catégorie</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,9 @@
-import Badge from 'react-bootstrap/Badge';
-import Button from 'react-bootstrap/Button';
-
 function Home() {
   return (
     <div>
       <h1 className="colortest">
         Hello world!
       </h1>
-
-      <h2>
-        Example heading
-        <Badge bg="secondary" as={Button}>
-          New
-        </Badge>
-      </h2>
 
     </div>
   );
