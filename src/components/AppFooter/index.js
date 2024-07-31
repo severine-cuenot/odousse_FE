487093ed41470d89de2f8a00939b3bec8f268ee0
Séverine Cuenot <nine.injections@gmail.com>
2024-07-31 11:43:40 +0200

    :rocket: feat: started footer

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,8 +1,20 @@
+// Imports React
+import { Link } from 'react-router-dom';
+
 import './style.scss';
 
 function AppFooter() {
   return (
-    <div>Hello Footer</div>
+    <footer className="bg-paon px-3 py-5 text-white">
+      <div>
+        <h5>Ôdousse</h5>
+        <p>Un modeste projet pour vous aider à trouver les locataires idéaux de votre aquarium</p>
+      </div>
+      <div>
+        <Link to="/about">A propos</Link>
+        <Link to="policy">Mentions légales</Link>
+      </div>
+    </footer>
   );
 }
 
