c56c2f40b5076402104aeca255b5d3f9b7577973
Séverine Cuenot <nine.injections@gmail.com>
2024-08-01 15:30:03 +0200

    :art: style: started to style footer

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -4,15 +4,34 @@ import { Link } from 'react-router-dom';
 import './style.scss';
 
 function AppFooter() {
+  const year = new Date().getFullYear();
   return (
-    <footer className="bg-paon px-3 py-5 text-white">
-      <div>
-        <h5>Ôdousse</h5>
-        <p>Un modeste projet pour vous aider à trouver les locataires idéaux de votre aquarium</p>
+    <footer className="bg-paon px-5 py-5">
+      <div className="footer__topBlock text-white">
+        <div className="footer__topBlock-left">
+          <h4>Ôdousse</h4>
+          <p>Un modeste projet pour vous aider à trouver les locataires idéaux de votre aquarium</p>
+        </div>
+        <div className="footer__topBlock-right">
+          <nav>
+            <h5>Les recherches par thèmes</h5>
+            <ul>
+              <li><Link to="/about">Biotopes</Link></li>
+              <li><Link to="policy">Litrage</Link></li>
+            </ul>
+          </nav>
+          <nav>
+            <h5>À propos</h5>
+            <ul>
+              <li><Link to="/about">Le projet</Link></li>
+              <li><Link to="policy">Mentions légales</Link></li>
+            </ul>
+          </nav>
+        </div>
       </div>
-      <div>
-        <Link to="/about">A propos</Link>
-        <Link to="policy">Mentions légales</Link>
+      <div className="footer__bottomBlock">
+        <hr />
+        <p>Tous droits réservés &copy; <a href="http://severine-cuenot.surge.sh/">Nine</a> {year}</p>
       </div>
     </footer>
   );
